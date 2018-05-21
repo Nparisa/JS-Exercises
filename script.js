367136d5ef9055d0b5907ea63eeacc1ddf2fdd83
Nparisa <parisanoushin@gmail.com>
2018-05-21 09:58:55 +0600

    task done

--- script.js
+++ script.js
@@ -1,1 +1,54 @@
 /* JavaScript code here */
+//task 1
+console.log("External JS");
+
+//task 2
+
+function sum(arr) {
+  var sum=0;
+        arr.forEach(function(value, index){
+          sum+=value;
+
+        });
+        return sum;
+        }
+
+        console.log(sum([1,2,3,4,5]));
+     
+
+//Task 3
+
+function ValidateEmail(email){
+  var emailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+  var match = emailFormat.test(email);
+  return match;
+}
+
+console.log(ValidateEmail("parsia@gmail.com"));
+
+
+//Task 4
+
+var library = [
+   {
+       author: 'Bill Gates',
+       title: 'The Road Ahead',
+       readingStatus: true
+   },
+   {
+       author: 'Steve Jobs',
+       title: 'Walter Isaacson',
+       readingStatus: true
+   },
+   {
+       author: 'Suzanne Collins',
+       title:  'Mockingjay: The Final Book of The Hunger Games',
+       readingStatus: false
+   }];
+
+function getReadingStatus(index){
+  return library[index].readingStatus;
+}
+
+console.log(getReadingStatus(2));
+
